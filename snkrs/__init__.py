ba97084dcd39e303e6aa00ffbd152fd9209306d1
very1fake <timurminer@gmail.com>
2020-03-27 12:23:21 +0300

    Nike SNKRS: Fix

--- snkrs/__init__.py
+++ snkrs/__init__.py
@@ -39,6 +39,7 @@ class Parser(api.Parser):
     def execute(self, target: api.TargetType) -> StatusType:
         try:
             if isinstance(target, api.TInterval):
+                model = 0
                 available: bool = False
                 content: dict = loads(
                     get(
@@ -46,12 +47,15 @@ class Parser(api.Parser):
                         headers={'user-agent': generate_user_agent()}
                     ).text
                 )
-                if content['productInfo'][0]['merchProduct']['publishType'] == 'FLOW':
-                    if datetime.strptime(content['productInfo'][0]['merchProduct']['commerceStartDate'],
+                for i, v in enumerate(content['productInfo']):
+                    if 'C' not in v['skus'][0]['nikeSize'] and 'Y' not in v['skus'][0]['nikeSize']:
+                        model = i
+                if content['productInfo'][model]['merchProduct']['publishType'] == 'FLOW':
+                    if datetime.strptime(content['productInfo'][model]['merchProduct']['commerceStartDate'],
                                          self.pattern).timestamp() < datetime.utcnow().timestamp():
                         available = True
-                elif content['productInfo'][0]['merchProduct']['publishType'] == 'LAUNCH':
-                    if datetime.strptime(content['productInfo'][0]['launchView']['startEntryDate'],
+                elif content['productInfo'][model]['merchProduct']['publishType'] == 'LAUNCH':
+                    if datetime.strptime(content['productInfo'][model]['launchView']['startEntryDate'],
                                          self.pattern).timestamp() < datetime.utcnow().timestamp():
                         available = True
                 else:
@@ -63,32 +67,34 @@ class Parser(api.Parser):
         except KeyError:
             return api.SFail(self.name, 'Wrong scheme')
         if available:
-            skus = [i.current_value for i in Path.parse_str('$.productInfo[0].availableSkus[*].id').match(content)]
+            skus = [i.current_value for i in
+                    Path.parse_str(f'$.productInfo[{model}].availableSkus[*].id').match(content)]
             return api.SSuccess(
                 self.name,
                 api.Result(
-                    content['productInfo'][0]['productContent']['title'],
+                    content['productInfo'][model]['productContent']['title'],
                     f'https://nike.com/ru/launch/t/{content["publishedContent"]["properties"]["seo"]["slug"]}',
                     'nike-snkrs',
-                    content['productInfo'][0]['imageUrls']['productImageUrl'],
-                    content['productInfo'][0]['productContent']['descriptionHeading'],
+                    content['productInfo'][model]['imageUrls']['productImageUrl'],
+                    content['productInfo'][model]['productContent']['descriptionHeading'],
                     (
                         api.currencies['ruble'],
-                        content['productInfo'][0]['merchPrice']['currentPrice'],
-                        content['productInfo'][0]['merchPrice']['fullPrice'] if
-                        content['productInfo'][0]['merchPrice']['discounted'] else 0
+                        content['productInfo'][model]['merchPrice']['currentPrice'],
+                        content['productInfo'][model]['merchPrice']['fullPrice'] if
+                        content['productInfo'][model]['merchPrice']['discounted'] else 0
                     ),
                     {},
                     tuple(
                         (
                             i['countrySpecifications'][0]['localizedSize'],
-                            f'https://www.nike.com/ru/launch/t/{content["productInfo"][0]["productContent"]["slug"]}'
-                            f'/?productId={content["productInfo"][0]["merchPrice"]["productId"]}'
+                            f'https://www.nike.com/ru/launch/t/'
+                            f'{content["publishedContent"]["properties"]["seo"]["slug"]}'
+                            f'/?productId={content["productInfo"][model]["merchPrice"]["productId"]}'
                             f'&size={i["countrySpecifications"][0]["localizedSize"].split(" ")[0]}'
-                        ) for i in content['productInfo'][0]['skus'] if i['id'] in skus),
+                        ) for i in content['productInfo'][model]['skus'] if i['id'] in skus),
                     (
                         ('StockX', 'https://stockx.com/search/sneakers?s=' +
-                         content['publishedContent']['nodes'][0]['properties']['altText']
+                         content['publishedContent']['nodes'][0]['nodes'][0]['properties']['altText']
                          .replace(' ', '%20').replace('\'', '').replace('‘', '')),
                         ('Feedback', 'https://forms.gle/9ZWFdf1r1SGp9vDLA')
                     )
