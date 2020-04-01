38e8cd7aa0414bf4dacca7f7e1e693a47afef773
duckha <vaneegorovu21@gmail.com>
2020-04-01 17:42:01 +0300

    Pharma

--- pharma/__init__.py
+++ pharma/__init__.py
@@ -3,7 +3,6 @@ from typing import List
 from lxml import etree
 from requests import get
 from user_agent import generate_user_agent
-from re import findall
 from jsonpath2 import Path
 from json import loads, JSONDecodeError
 
@@ -15,31 +14,30 @@ from core.logger import Logger
 class Parser(api.Parser):
     def __init__(self, name: str, log: Logger):
         super().__init__(name, log)
-        self.catalog: str = 'https://extrabutterny.com/collections/footwear/Mens'
+        self.catalog: str = 'https://shop.pharmabergen.no/collections/new-arrivals/'
         self.interval: int = 1
         self.user_agent = generate_user_agent()
 
     def index(self) -> IndexType:
-        return api.IInterval(self.name, 15)
+        return api.IInterval(self.name, 10)
 
     def targets(self) -> List[TargetType]:
         return [
             api.TInterval(element[0].get('href').split('/')[4],
-                          self.name, 'https://extrabutterny.com' + element[0].get('href'), self.interval)
-            for element in etree.HTML(get(self.catalog,
+                          self.name, 'https://shop.pharmabergen.no' + element[0].get('href'), self.interval)
+            for element in etree.HTML(get('https://shop.pharmabergen.no/collections/new-arrivals/',
                                           headers={'user-agent': generate_user_agent()}
-                                          ).text).xpath('//div[@class="GridItem-imageContainer"]')
-            if
-            'nike' in element[0].get('href') or 'jordan' in element[0].get('href') or 'yeezy' in element[0].get('href')
+                                          ).text).xpath('//div[@class="product-info-inner"]')
+            if element[0].xpath('span[@class]')[0].text in ['NIKE', 'JORDAN'] or 'yeezy' in element[0].get('href')
         ]
 
     def execute(self, target: TargetType) -> StatusType:
         try:
             if isinstance(target, api.TInterval):
                 available: bool = False
-                get_content = get(target.data, headers={'user-agent': generate_user_agent()}).text
-                content: etree.Element = etree.HTML(get_content)
-                if content.xpath('//strong')[0].text.replace(' ', '').replace('\t', '').replace('\n', '') != 'SoldOut':
+                content: etree.Element = etree.HTML(
+                    get(target.data, headers={'user-agent': generate_user_agent()}).text)
+                if content.xpath('//input[@type="submit"]')[0].get('value').replace('\n', '') == 'Add to Cart':
                     available = True
                 else:
                     return api.SWaiting(target)
@@ -49,10 +47,9 @@ class Parser(api.Parser):
             return api.SFail(self.name, 'Exception XMLDecodeError')
         if available:
             try:
-                sizes_data = Path.parse_str('$.product.variants.*').match(
-                    loads(findall(r'var meta = {.*}', get_content)[0]
-                          .replace('var meta = ', '')))
-                name = content.xpath('//title')[0].text.split(' [')[0]
+                sizes_data = Path.parse_str('$.variants.*').match(
+                    loads(content.xpath('//form[@action="/cart/add"]')[0].get('data-product')))
+                name = content.xpath('//meta[@property="og:title"]')[0].get('content')
                 return api.SSuccess(
                     self.name,
                     api.Result(
@@ -62,13 +59,14 @@ class Parser(api.Parser):
                         content.xpath('//meta[@property="og:image:secure_url"]')[0].get('content'),
                         '',
                         (
-                            api.currencies['ruble'],
-                            float(content.xpath('//meta[@property="og:price:amount"]')[0].get('content'))
+                            api.currencies['dollar'],
+                            float(content.xpath('//meta[@property="og:price:amount"]')[0].get('content')
+                                  .replace('.','').replace(',','.'))
                         ),
                         {},
                         tuple(
                             (
-                                size_data.current_value['public_title'],
+                                size_data.current_value['option1'],
                                 target.data + '?variant=' + str(size_data.current_value['id'])
                             ) for size_data in sizes_data
                         ),
@@ -82,3 +80,4 @@ class Parser(api.Parser):
                 return api.SFail(self.name, 'Exception XMLDecodeError')
         else:
             return api.SWaiting(target)
+
