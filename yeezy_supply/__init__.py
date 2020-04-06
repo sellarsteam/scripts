3b4669414875eb68f7e96355dd4737159c73e80d
duckha <vaneegorovu21@gmail.com>
2020-04-06 12:49:51 +0300

    Fix supply, and opimize supreme, traektoria, and add proxies

--- yeezy_supply/__init__.py
+++ yeezy_supply/__init__.py
@@ -3,96 +3,52 @@ from typing import List
 
 from jsonpath2 import Path
 from requests import get
-from requests.exceptions import ReadTimeout
-from user_agent import generate_user_agent
+from scripts.proxy import get_proxy
 
 from core import api
 from core.api import IndexType, TargetType, StatusType
 from core.logger import Logger
+
+headers = {
+    'user-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:71.0) Gecko/20100101 Firefox/71.0',
+    'referer': 'https://www.yeezysupply.com',
+    'accept-encoding': 'gzip, deflate, br',
+    'connection': 'keep-alive',
+    'host': 'www.yeezysupply.com',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'accept-language': 'en-US,en;q=0.5',
+}
 
 
 class Parser(api.Parser):
     def __init__(self, name: str, log: Logger):
         super().__init__(name, log)
         self.catalog: str = 'https://www.yeezysupply.com/api/yeezysupply/products/bloom'
-        self.user_agent = generate_user_agent()
         self.interval: int = 1
 
     def index(self) -> IndexType:
-        return api.IInterval(self.name, 120)
+        return api.IInterval(self.name, 10)
+
 
     def targets(self) -> List[TargetType]:
-        while True:
-            try:
-                return [
-                    api.TInterval(
-                        i.current_value['product_name'],
-                        self.name,
-                        'https://www.yeezysupply.com/api/products/' + i.current_value['product_id'],
-                        self.interval
-                    )
-                    for i in Path.parse_str('$[*]').match(
-                        loads(get(self.catalog, headers={
-                            'user-agent': self.user_agent,
-                            'referer': 'https://www.yeezysupply.com',
-                            'connection': 'keep-alive',
-                            'cache-control': 'max-age=0', 'upgrade-insecure-requests': '1',
-                            'sec-fetch-dest': 'document',
-                            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-                            'sec-fetch-site': 'same-origin', 'sec-fetch-mode': 'navigate',
-                            'sec-fetch-user': '?1',
-                            'accept-language': 'en-US,en;q=0.9',
-                        }, timeout=3).text)
-                    )
-                ]
-            except ReadTimeout:
-                pass
+        return [
+            api.TInterval(
+                i.current_value['product_id'],
+                self.name,
+                'https://www.yeezysupply.com/api/products/' + i.current_value['product_id'],
+                self.interval
+            )
+            for i in Path.parse_str('$[*]').match(
+                loads(get(self.catalog, headers=headers, proxies=get_proxy()).text)
+            )
+        ]
 
     def execute(self, target: TargetType) -> StatusType:
         try:
             if isinstance(target, api.TInterval):
-                while True:
-                    try:
-                        sizes_json = Path.parse_str('$.variation_list[*]').match(loads(
-                            get(f'https://www.yeezysupply.com/api/products/{target.data.split("/")[5]}/availability',
-                                headers={
-                                    'user-agent': self.user_agent,
-                                    'referer': f'https://www.yeezysupply.com/product/{target.data.split("/")[5]}',
-                                    'connection': 'keep-alive', 'cache-control': 'max-age=0',
-                                    'upgrade-insecure-requests': '1', 'sec-fetch-dest': 'document',
-                                    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-                                    'sec-fetch-site': 'same-origin', 'sec-fetch-mode': 'navigate',
-                                    'sec-fetch-user': '?1',
-                                    'accept-language': 'en-US,en;q=0.9'
-                                }, timeout=3).text))
-                        break
-                    except ReadTimeout:
-                        pass
-
                 available: bool = False
-                while True:
-                    try:
-                        content: dict = loads(
-                            get(
-                                target.data,
-                                headers={
-                                    'user-agent': generate_user_agent(),
-                                    'referer': 'https://www.yeezysupply.com',
-                                    'connection': 'keep-alive', 'cache-control': 'max-age=0',
-                                    'upgrade-insecure-requests': '1', 'sec-fetch-dest': 'document',
-                                    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-                                    'sec-fetch-site': 'same-origin', 'sec-fetch-mode': 'navigate',
-                                    'sec-fetch-user': '?1',
-                                    'accept-language': 'en-US,en;q=0.9'
-                                }
-                                , timeout=3
-                            ).text
-                        )
-                        break
-                    except ReadTimeout:
-                        pass
-                if tuple(size.current_value for size in sizes_json) != ():
-                    available = True
+                content: dict = loads(get(target.data, headers=headers, proxies=get_proxy()).text)
+                available = True
             else:
                 return api.SFail(self.name, 'Unknown target type')
         except JSONDecodeError:
@@ -100,6 +56,9 @@ class Parser(api.Parser):
         except KeyError:
             return api.SFail(self.name, 'Wrong scheme')
         if available:
+            sizes_json = Path.parse_str('$.variation_list.*').match(loads(
+                    get(f'https://www.yeezysupply.com/api/products/{target.name}/availability',
+                        headers=headers, proxies=get_proxy()).text))
             return api.SSuccess(
                 self.name,
                 api.Result(
@@ -110,7 +69,8 @@ class Parser(api.Parser):
                     content['meta_data']['description'],
                     (api.currencies['USD'], float(content['pricing_information']['standard_price'])),
                     {},
-                    tuple(size.current_value['size'] for size in sizes_json),
+                    tuple(size.current_value['size'] + ' US' + ' [' + str(size.current_value['availability']) + ']' for size in sizes_json
+                          if size.current_value['availability'] > 0),
                     ()
                 )
             )
