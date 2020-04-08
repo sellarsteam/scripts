4a561195de87f34bea4ccbe6800aee0d3c1b91e6
duckha <vaneegorovu21@gmail.com>
2020-04-08 14:54:25 +0300

    Fix Hibbet

--- hibbett/__init__.py
+++ hibbett/__init__.py
@@ -2,8 +2,9 @@ import re
 from typing import List
 
 from lxml import etree
-from requests import get
 from user_agent import generate_user_agent
+from cfscrape import create_scraper
+from scripts.proxy import get_proxy
 
 from core import api
 from core.api import IndexType, TargetType, StatusType
@@ -13,44 +14,28 @@ from core.logger import Logger
 class Parser(api.Parser):
     def __init__(self, name: str, log: Logger):
         super().__init__(name, log)
-        self.catalog: str = 'https://www.hibbett.com/launch-calendar/?prefn1=dtLaunch&prefv1=-120&srule=launch-date-desc'
+        self.scraper = create_scraper()
+        self.catalog: str = 'https://www.hibbett.com/launch-calendar/?prefn1=dtLaunch&prefv1=30'
         self.interval: int = 1
         self.user_agent = generate_user_agent()
 
     def index(self) -> IndexType:
-        return api.IInterval(self.name, 120)
+        return api.IInterval(self.name, 10)
 
     def targets(self) -> List[TargetType]:
         return [
             api.TInterval(element.get('href').split('/')[3],
                           self.name, element.get('href'), self.interval)
-            for element in etree.HTML(get(
-                self.catalog,
-                headers={'user-agent': self.user_agent,
-                         'connection': 'keep-alive', 'cache-control': 'max-age=0',
-                         'upgrade-insecure-requests': '1', 'sec-fetch-dest': 'document',
-                         'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-                         'sec-fetch-site': 'same-origin', 'sec-fetch-mode': 'navigate',
-                         'sec-fetch-user': '?1',
-                         'accept-language': 'en-US,en;q=0.9'}
-            ).text).xpath('//a[@class="name-link"]')
+            for element in etree.HTML(self.scraper.get(
+                url=self.catalog, proxies=get_proxy()
+            ).text).xpath('//a[@class="name-link"]') if 'dunk' in element.get('href') or 'yeezy' in element.get('href') or 'jordan' in element.get('href') or 'sacai' in element.get('href') or 'air' in element.get('href')
         ]
 
     def execute(self, target: TargetType) -> StatusType:
         try:
             if isinstance(target, api.TInterval):
                 available: bool = False
-                content: etree.Element = etree.HTML(get(
-                    target.data,
-                    headers={'user-agent': generate_user_agent(),
-                             'connection': 'keep-alive', 'cache-control': 'max-age=0',
-                             'upgrade-insecure-requests': '1', 'sec-fetch-dest': 'document',
-                             'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-                             'sec-fetch-site': 'same-origin', 'sec-fetch-mode': 'navigate',
-                             'sec-fetch-user': '?1',
-                             'accept-language': 'en-US,en;q=0.9',
-                             'referer': self.catalog
-                             }).text)
+                content: etree.Element = etree.HTML(self.scraper.get(url=target.data, proxies=get_proxy()).text)
 
                 if len(content.xpath('//a[@class="swatchanchor"]')) > 0:
                     available = True
