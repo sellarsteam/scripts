017fea8ff88dde4625a0ce5e1372c4f17f2f1b08
duckha <vaneegorovu21@gmail.com>
2020-04-09 22:48:43 +0300

    Fix Footpatrol-us

--- footpatrol_us/__init__.py
+++ footpatrol_us/__init__.py
@@ -2,8 +2,8 @@ import re
 from typing import List
 
 from lxml import etree
-from requests import get
-from user_agent import generate_user_agent
+from cfscrape import create_scraper
+from scripts.proxy import get_proxy
 
 from core import api
 from core.api import IndexType, TargetType, StatusType
@@ -15,42 +15,30 @@ class Parser(api.Parser):
         super().__init__(name, log)
         self.catalog: str = 'https://www.footpatrol.com/campaign/New+In/brand/nike,jordan,adidas-originals/latest/?facet-new=latest&fp_sort_order=latest'
         self.interval: int = 1
-        self.user_agent = generate_user_agent()
+        self.scrapper = create_scraper()
+
 
     def index(self) -> IndexType:
-        return api.IInterval(self.name, 30)
+        return api.IInterval(self.name, 10)
 
     def targets(self) -> List[TargetType]:
         return [
             api.TInterval(element.get('href').split('/')[2],
                           self.name, 'https://www.footpatrol.com' + element.get('href'), self.interval)
-            for element in etree.HTML(get(
-                self.catalog,
-                headers={'user-agent': self.user_agent,
-                         'connection': 'keep-alive', 'cache-control': 'max-age=0',
-                         'upgrade-insecure-requests': '1', 'sec-fetch-dest': 'document',
-                         'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-                         'sec-fetch-site': 'same-origin', 'sec-fetch-mode': 'navigate',
-                         'sec-fetch-user': '?1',
-                         'accept-language': 'en-US,en;q=0.9'}
-            ).text).xpath('//a[@data-e2e="product-listing-name"]')
+            for element in etree.HTML(self.scrapper.get(
+                self.catalog, proxies=get_proxy()).text).xpath('//a[@data-e2e="product-listing-name"]') if 'yeezy' in element.get('href') or
+                                                                                      'jordan' in element.get('href') or
+                                                                                      'air' in element.get('href') or
+                                                                                      'sacai' in element.get('href') or
+                                                                                      'zoom' in element.get('href') or
+                                                                                      'dunk' in element.get('href')
         ]
 
     def execute(self, target: TargetType) -> StatusType:
         try:
             if isinstance(target, api.TInterval):
                 available: bool = False
-                content: etree.Element = etree.HTML(get(
-                    target.data,
-                    headers={'user-agent': generate_user_agent(),
-                             'connection': 'keep-alive', 'cache-control': 'max-age=0',
-                             'upgrade-insecure-requests': '1', 'sec-fetch-dest': 'document',
-                             'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-                             'sec-fetch-site': 'same-origin', 'sec-fetch-mode': 'navigate',
-                             'sec-fetch-user': '?1',
-                             'accept-language': 'en-US,en;q=0.9',
-                             'referer': 'https://www.footpatrol.com/campaign/New+In/brand/nike,jordan,adidas-originals/latest/?facet-new=latest&fp_sort_order=latest'
-                             }).text)
+                content: etree.Element = etree.HTML(self.scrapper.get(target.data, proxies=get_proxy()).text)
 
                 if content.xpath('//button[@id="addToBasket"]') != []:
                     available = True
@@ -87,3 +75,5 @@ class Parser(api.Parser):
             )
         else:
             return api.SWaiting(target)
+
+
