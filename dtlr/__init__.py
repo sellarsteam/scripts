7ab3aaaa756279479c64f49bcb1fde1fccb07aa1
duckha <vaneegorovu21@gmail.com>
2020-04-25 23:42:44 +0300

    Add DLTR Script

--- dtlr/__init__.py
+++ dtlr/__init__.py
@@ -14,45 +14,36 @@ from core.logger import Logger
 class Parser(api.Parser):
     def __init__(self, name: str, log: Logger):
         super().__init__(name, log)
-        self.catalog: str = 'https://kith.com/collections/mens-footwear'
-        self.interval: float = 1
+        self.catalog: str = 'https://www.dtlr.com/pages/releases'
+        self.interval: int = 1
+        self.user_agent = 'Pinterest/0.2 (+https://www.pinterest.com/bot.html)Mozilla/5.0 (compatible; ' \
+                          'Pinterestbot/1.0; +https://www.pinterest.com/bot.html)Mozilla/5.0 (Linux; ' \
+                          'Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) ' \
+                          'Chrome/41.0.2272.96 Mobile Safari/537.36 (compatible; Pinterestbot/1.0; ' \
+                          '+https://www.pinterest.com/bot.html)'
 
     def index(self) -> IndexType:
         return api.IInterval(self.name, 1)
 
     def targets(self) -> List[TargetType]:
         return [
-            api.TInterval(element.xpath('a')[0].get('href').split('/')[2],
-                          self.name, 'https://kith.com/' + element.xpath('a')[0].get('href'), self.interval)
+            api.TInterval(element.get('href').split('/')[2],
+                          self.name, 'https://www.dtlr.com' + element.get('href'), self.interval)
             for element in etree.HTML(get(
                 self.catalog,
-                headers={'user-agent': 'Pinterest/0.2 (+https://www.pinterest.com/bot'
-                                       '.html)Mozilla/5.0 (compatible; '
-                                       'Pinterestbot/1.0; '
-                                       '+https://www.pinterest.com/bot.html)Mozilla/5'
-                                       '.0 (Linux; Android 6.0.1; Nexus 5X '
-                                       'Build/MMB29P) AppleWebKit/537.36 (KHTML, '
-                                       'like Gecko) Chrome/41.0.2272.96 Mobile '
-                                       'Safari/537.36 (compatible; Pinterestbot/1.0; '
-                                       '+https://www.pinterest.com/bot.html)'}
-            ).text).xpath('//div[@class="product-card__information"]')
-            if 'Nike' in element[0].xpath('h1[@class="product-card__title"]')[0].text
-               or 'Yeezy' in element[0].xpath('h1[@class="product-card__title"]')[0].text 
-               or 'Jordan' in element[0].xpath('h1[@class="product-card__title"]')[0].text
+                headers={'user-agent': self.user_agent}
+            ).text).xpath(
+                '//div[@class="release-item adidas-logo" or @class="release-item jordan-logo" or '
+                '@class="release-item nike-logo"]/a')
+            if 'yeezy' in element.get('href') or 'Jordan' in element.get('href') \
+               or 'jordan' in element.get('href') or 'nike' in element.get('href') \
+               or 'Nike' in element.get('href')
         ]
 
     def execute(self, target: TargetType) -> StatusType:
         try:
             if isinstance(target, api.TInterval):
-                get_content = get(target.data, headers={'user-agent': 'Pinterest/0.2 (+https://www.pinterest.com/bot'
-                                                                      '.html)Mozilla/5.0 (compatible; '
-                                                                      'Pinterestbot/1.0; '
-                                                                      '+https://www.pinterest.com/bot.html)Mozilla/5'
-                                                                      '.0 (Linux; Android 6.0.1; Nexus 5X '
-                                                                      'Build/MMB29P) AppleWebKit/537.36 (KHTML, '
-                                                                      'like Gecko) Chrome/41.0.2272.96 Mobile '
-                                                                      'Safari/537.36 (compatible; Pinterestbot/1.0; '
-                                                                      '+https://www.pinterest.com/bot.html)'}).text
+                get_content = get(target.data, headers={'user-agent': self.user_agent}).text
                 content: etree.Element = etree.HTML(get_content)
             else:
                 return api.SFail(self.name, 'Unknown target type')
@@ -63,8 +54,6 @@ class Parser(api.Parser):
             return api.SFail(self.name, 'Exception XMLDecodeError')
         except JSONDecodeError:
             return api.SFail(self.name, 'Exception JSONDecodeError')
-
-
         except IndexError:  # TODO return info, that target is sold out
             return api.SSuccess(
                 self.name,
@@ -81,16 +70,33 @@ class Parser(api.Parser):
                      ('Feedback', 'https://forms.gle/9ZWFdf1r1SGp9vDLA'))
                 )
             )
-
         name = content.xpath('//meta[@property="og:title"]')[0].get('content').split(' -')[0]
-        available_sizes = tuple(
-            (element.get('data-value')) for element in content.xpath('//div[@class="swatch clearfix"]')[0].xpath('div'))
+        try:
+            available_sizes = tuple(
+                (element.get('data-value')) for element in content.xpath('//div[@class="swatch  clearfix"]')[0].xpath('div')
+                if 'available' in element.get('class'))
+        except IndexError:  # TODO return info, that target is sold out
+            return api.SSuccess(
+                self.name,
+                api.Result(
+                    'Sold out',
+                    target.data,
+                    'tech',
+                    '',
+                    '',
+                    (api.currencies['USD'], 1),
+                    {},
+                    tuple(),
+                    (('StockX', 'https://stockx.com/search/sneakers?s='),
+                     ('Feedback', 'https://forms.gle/9ZWFdf1r1SGp9vDLA'))
+                )
+            )
         return api.SSuccess(
             self.name,
             api.Result(
                 name,
                 target.data,
-                'kith',
+                'shopify-filtered',
                 content.xpath('//meta[@property="og:image"]')[0].get('content'),
                 '',
                 (
@@ -101,7 +107,7 @@ class Parser(api.Parser):
                 tuple(
                     (
                         str(size_data.current_value['public_title']) + ' US',
-                        'https://kith.com/cart/' + str(size_data.current_value['id']) + ':1'
+                        'https://www.dtlr.com/cart/' + str(size_data.current_value['id']) + ':1'
                     ) for size_data in sizes_data if size_data.current_value['public_title'] in available_sizes
                 ),
                 (
@@ -110,3 +116,4 @@ class Parser(api.Parser):
                 )
             )
         )
+
