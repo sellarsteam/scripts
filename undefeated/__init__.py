daef34445746ba64a01efea4c0f5842dd7d8f8e2
duckha <vaneegorovu21@gmail.com>
2020-04-30 20:18:41 +0300

    Fix undefeated

--- undefeated/__init__.py
+++ undefeated/__init__.py
@@ -36,7 +36,11 @@ class Parser(api.Parser):
         super().__init__(name, log)
         self.catalog: str = 'https://undefeated.com/collections/mens-footwear'
         self.interval: int = 1
-        self.user_agent = generate_user_agent()
+        self.user_agent = 'Pinterest/0.2 (+https://www.pinterest.com/bot.html)Mozilla/5.0 ' \
+                          '(compatible; Pinterestbot/1.0; +https://www.pinterest.com/bot.html)' \
+                          'Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 ' \
+                          '(KHTML, like Gecko) Chrome/41.0.2272.96 Mobile Safari/537.36 (compatible; ' \
+                          'Pinterestbot/1.0; +https://www.pinterest.com/bot.html)'
 
     def index(self) -> IndexType:
         return api.IInterval(self.name, 1)
@@ -46,15 +50,7 @@ class Parser(api.Parser):
             api.TInterval(element[0].xpath('a')[0].get('href').split('/')[4],
                           self.name, 'https://undefeated.com' + element[0].xpath('a')[0].get('href'), self.interval)
             for element in etree.HTML(get(self.catalog,
-                                          headers={'user-agent': 'Pinterest/0.2 (+https://www.pinterest.com/bot'
-                                                                      '.html)Mozilla/5.0 (compatible; '
-                                                                      'Pinterestbot/1.0; '
-                                                                      '+https://www.pinterest.com/bot.html)Mozilla/5'
-                                                                      '.0 (Linux; Android 6.0.1; Nexus 5X '
-                                                                      'Build/MMB29P) AppleWebKit/537.36 (KHTML, '
-                                                                      'like Gecko) Chrome/41.0.2272.96 Mobile '
-                                                                      'Safari/537.36 (compatible; Pinterestbot/1.0; '
-                                                                      '+https://www.pinterest.com/bot.html)'}
+                                          headers={'user-agent': self.user_agent}
                                           ).text).xpath('//div[@class="grid-product__wrapper"]')
             if 'air' in element[0].xpath('a')[0].get('href') or 'yeezy' in element[0].xpath('a')[0].get('href')
                or 'aj' in element[0].xpath('a')[0].get('href') or 'dunk' in element[0].xpath('a')[0].get('href')
@@ -64,19 +60,9 @@ class Parser(api.Parser):
         try:
             if isinstance(target, api.TInterval):
                 available: bool = False
-                get_content = get(target.data, headers={'user-agent': 'Pinterest/0.2 (+https://www.pinterest.com/bot'
-                                                                      '.html)Mozilla/5.0 (compatible; '
-                                                                      'Pinterestbot/1.0; '
-                                                                      '+https://www.pinterest.com/bot.html)Mozilla/5'
-                                                                      '.0 (Linux; Android 6.0.1; Nexus 5X '
-                                                                      'Build/MMB29P) AppleWebKit/537.36 (KHTML, '
-                                                                      'like Gecko) Chrome/41.0.2272.96 Mobile '
-                                                                      'Safari/537.36 (compatible; Pinterestbot/1.0; '
-                                                                      '+https://www.pinterest.com/bot.html)'}).text
-
+                get_content = get(target.data, headers={'user-agent': self.user_agent}).text
                 content: etree.Element = etree.HTML(get_content)
-
-                if content.xpath('//span[@class="btn__text"]')[0].text.replace(' ', '').replace('\n', '') != 'SoldOut':
+                if content.xpath('//span[@class="btn__text"]')[0].text.replace(' ', '').replace('\n', '') != 'Sold':
                     available = True
                 else:
                     return return_sold_out(target.data)
