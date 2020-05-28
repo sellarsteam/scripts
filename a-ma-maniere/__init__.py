f78bd183955eb862a366ab03695ec65e4c591031
duckha <vaneegorovu21@gmail.com>
2020-05-28 23:05:17 +0300

    Optimized a-ma-maniere script for new API

--- a-ma-maniere/__init__.py
+++ a-ma-maniere/__init__.py
@@ -1,19 +1,21 @@
 from json import loads, JSONDecodeError
 from re import findall
-from typing import List
+from typing import List, Union
 
 from jsonpath2 import Path
 from lxml import etree
 
 from source import api
-from source.api import IndexType, TargetType, StatusType
-from source.logger import Logger
+from source import logger
+from source.api import CatalogType, TargetType, RestockTargetType, ItemType, TargetEndType, IRelease, FooterItem
+from source.cache import HashStorage
+from source.library import SubProvider
 
 
 class Parser(api.Parser):
-    def __init__(self, name: str, log: Logger, provider: api.SubProvider, storage):
-        super().__init__(name, log, provider, storage)
-        self.catalog: str = 'https://www.a-ma-maniere.com/collections/new-arrivals'
+    def __init__(self, name: str, log: logger.Logger, provider_: SubProvider):
+        super().__init__(name, log, provider_)
+        self.link: str = 'https://www.a-ma-maniere.com/collections/new-arrivals'
         self.interval: int = 1
         self.user_agent = 'Pinterest/0.2 (+https://www.pinterest.com/bot.html)Mozilla/5.0 (compatible; ' \
                           'Pinterestbot/1.0; +https://www.pinterest.com/bot.html)Mozilla/5.0 (Linux; Android ' \
@@ -21,95 +23,74 @@ class Parser(api.Parser):
                           'Chrome/41.0.2272.96 Mobile Safari/537.36 (compatible; ' \
                           'Pinterestbot/1.0; +https://www.pinterest.com/bot.html)'
 
-    def index(self) -> IndexType:
-        return api.IInterval(self.name, 3)
+    @property
+    def catalog(self) -> CatalogType:
+        return api.CInterval(self.name, 3.)
 
-    def targets(self) -> List[TargetType]:
-        links = list()
-        counter = 0
-        for element in etree.HTML(self.provider.get(self.catalog,
-                                                    headers={'user-agent': self.user_agent}, proxy=True)) \
-                .xpath('//div[@class="collection-product"]/a'):
-            if counter == 5:
-                break
-            if 'yeezy' in element.get('href') or 'air' in element.get('href') or 'dunk' in element.get('href') \
-                    or 'dunk' in element.get('href') or 'retro' in element.get('href') \
-                    or 'blazer' in element.get('href'):
-                links.append(element.get('href'))
-            counter += 1
-        return [
-            api.TInterval(element.split('/')[-1],
-                          self.name, 'https://www.a-ma-maniere.com' + element, self.interval)
-            for element in links
-        ]
-
-    def execute(self, target: TargetType) -> StatusType:
-        try:
-            if isinstance(target, api.TInterval):
-                get_content = self.provider.get(target.data, headers={'user-agent': self.user_agent}, proxy=True)
-                content: etree.Element = etree.HTML(get_content)
-                counter = 0
-                symbol = 'c'
-                available_sizes = list()
-                for size in content.xpath('//div[@class="product-size-container"]/span'):
-                    if counter == 0:
-                        symbol = size.text[-1]
-                        counter += 1
-                    if 'unavailable' not in size.get('class'):
-                        available_sizes.append(size.text.replace(symbol, ''))
-                sizes = list()
-                for size in Path.parse_str('$.product.variants.*').match(
-                        loads(findall(r'var meta = {.*}', get_content)[0].replace('var meta = ', ''))):
-                    if size.current_value['public_title'] in available_sizes:
-                        sizes.append(
-                            (
-                                str(size.current_value['public_title']) + symbol,
-                                'https://www.a-ma-maniere.com/cart/' + str(size.current_value['id']) + ':1'
-                            )
-                        )
-            else:
-                return api.SFail(self.name, 'Unknown target type')
-        except etree.XMLSyntaxError:
-            return api.SFail(self.name, 'Exception XMLDecodeError')
-        except JSONDecodeError:
-            return api.SFail(self.name, 'Exception JSONDecodeError')
-        if len(available_sizes) > 0:
-            name = content.xpath('//meta[@property="og:title"]')[0].get('content')
-            return api.SSuccess(
-                self.name,
-                api.Result(
-                    name,
-                    target.data,
-                    'shopify-filtered',
-                    content.xpath('//meta[@property="og:image"]')[0].get('content'),
-                    '',
-                    (
-                        api.currencies['USD'],
-                        float(content.xpath('//meta[@property="og:price:amount"]')[0].get('content'))
-                    ),
-                    {'Site': 'A-Ma-Maniere'},
-                    tuple(sizes),
-                    (
-                        ('StockX', 'https://stockx.com/search/sneakers?s=' + name.replace(' ', '%20')),
-                        ('Cart', 'https://www.a-ma-maniere.com/cart'),
-                        ('Feedback', 'https://forms.gle/9ZWFdf1r1SGp9vDLA')
-                    )
-                )
-            )
-        else:  # TODO return info, that target is sold out
-            return api.SSuccess(
-                self.name,
-                api.Result(
-                    'Sold out',
-                    target.data,
-                    'tech',
-                    '',
-                    '',
-                    (api.currencies['USD'],
-                     float(1)),
-                    {},
-                    tuple(),
-                    (('StockX', 'https://stockx.com/search/sneakers?s='),
-                     ('Feedback', 'https://forms.gle/9ZWFdf1r1SGp9vDLA'))
-                )
-            )
+    def execute(self, mode: int, content: Union[CatalogType, TargetType]) -> List[
+        Union[CatalogType, TargetType, RestockTargetType, ItemType, TargetEndType]]:
+        if mode == 0:
+            links = list()
+            counter = 0
+            for element in etree.HTML(self.provider.get(self.link,
+                                                        headers={'user-agent': self.user_agent}, proxy=True)) \
+                    .xpath('//div[@class="collection-product"]/a'):
+                if counter == 5:
+                    break
+                if 'yeezy' in element.get('href') or 'air' in element.get('href') or 'dunk' in element.get('href') \
+                        or 'dunk' in element.get('href') or 'retro' in element.get('href') \
+                        or 'blazer' in element.get('href'):
+                    links.append([api.Target('https://www.a-ma-maniere.com' + element.get('href'), self.name, 0),
+                                  'https://www.a-ma-maniere.com' + element.get('href')])
+                counter += 1
+            if len(links) == 0:
+                return [content]
+            for link in links:
+                try:
+                    if HashStorage.check_target(link[0].hash()):
+                        item_link = link[1]
+                        get_content = self.provider.get(item_link, headers={'user-agent': self.user_agent}, proxy=True)
+                        page_content = etree.Element = etree.HTML(get_content)
+                        counter = 0
+                        symbol = 'c'
+                        available_sizes = list()
+                        for size in page_content.xpath('//div[@class="product-size-container"]/span'):
+                            if counter == 0:
+                                symbol = size.text[-1]
+                                counter += 1
+                            if 'unavailable' not in size.get('class'):
+                                available_sizes.append(size.text.replace(symbol, ''))
+                        sizes = list()
+                        for size in Path.parse_str('$.product.variants.*').match(
+                                loads(findall(r'var meta = {.*}', get_content)[0].replace('var meta = ', ''))):
+                            value = size.current_value['public_title']
+                            if value in available_sizes:
+                                sizes.append(api.Size(str(size.current_value['public_title']) + symbol,
+                                                      'https://www.a-ma-maniere.com/cart/' + str(value) + ':1'))
+                        name = page_content.xpath('//meta[@property="og:title"]')[0].get('content')
+                        HashStorage.add_target(link[0].hash())
+                        return [IRelease(
+                            name,
+                            item_link,
+                            'shopify-filtered',
+                            page_content.xpath('//meta[@property="og:image"]')[0].get('content'),
+                            '',
+                            api.Price(
+                                api.CURRENCIES['USD'],
+                                float(page_content.xpath('//meta[@property="og:price:amount"]')[0].get('content'))
+                            ),
+                            api.Sizes(api.SIZE_TYPES[''], sizes),
+                            [
+                                FooterItem('StockX',
+                                           'https://stockx.com/search/sneakers?s=' + name.replace(' ', '%20')),
+                                FooterItem('Cart', 'https://www.a-ma-maniere.com/cart'),
+                                FooterItem('Feedback', 'https://forms.gle/9ZWFdf1r1SGp9vDLA')
+                            ],
+                            {'Site': 'A-Ma-Maniere'}
+                        ), content]
+                    else:
+                        continue
+                except etree.XMLSyntaxError:
+                    raise etree.XMLSyntaxError('Exception XMLDecodeError')
+                except JSONDecodeError:
+                    raise JSONDecodeError('Exception JSONDecodeError')
