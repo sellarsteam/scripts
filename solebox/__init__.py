5209debf5373873df4d5cb605066bf2a923c6ccd
duckha <vaneegorovu21@gmail.com>
2020-05-31 17:23:06 +0300

    Optimized solebox script for new API

--- solebox/__init__.py
+++ solebox/__init__.py
@@ -1,86 +1,118 @@
-from typing import List
+from typing import List, Union
 
 from lxml import etree
 
 from source import api
-from source.api import IndexType, TargetType, StatusType
-from source.logger import Logger
+from source import logger
+from source.api import CatalogType, TargetType, RestockTargetType, ItemType, TargetEndType, IRelease, FooterItem
+from source.library import SubProvider
 
 
 class Parser(api.Parser):
-    def __init__(self, name: str, log: Logger, provider: api.SubProvider, storage):
-        super().__init__(name, log, provider, storage)
-        self.catalog: str = 'https://www.solebox.com/de_DE/c/new'
+    def __init__(self, name: str, log: logger.Logger, provider_: SubProvider):
+        super().__init__(name, log, provider_)
+        self.link: str = 'https://www.solebox.com/en_RU/c/footwear?prefn1=isNew&prefv1=true&openCategory=true' \
+                         '&specificCategory=new '
         self.interval: int = 1
         self.user_agent = 'Pinterest/0.2 (+https://www.pinterest.com/bot .html)Mozilla/5.0 (compatible; ' \
                           'Pinterestbot/1.0; +https://www.pinterest.com/bot.html)Mozilla/5.0 (Linux; Android 6.0.1; ' \
                           'Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.96 Mobile ' \
                           'Safari/537.36 (compatible; Pinterestbot/1.0; +https://www.pinterest.com/bot.html) '
+        self.headers = {'authority': 'www.solebox.com',
+                        'scheme': 'https',
+                        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,'
+                                  '*/*;q=0.8,application/signed-exchange;v=b3',
+                        'accept-encoding': 'gzip, deflate, br',
+                        'accept-language': 'it-IT,it;q=0.9,en-US;q=0.8,en;q=0.7',
+                        'cache-control': 'max-age=0',
+                        'upgrade-insecure-requests': '1',
+                        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, '
+                                      'like Gecko) Chrome/73.0.3683.103 Safari/537.36'}
 
-    def index(self) -> IndexType:
-        return api.IInterval(self.name, 3)
+    @property
+    def catalog(self) -> CatalogType:
+        return api.CInterval(self.name, 1200.)
 
-    def targets(self) -> List[TargetType]:
-        return [
-            api.TInterval(element.get('href').split('/')[3],
-                          self.name, 'https://www.solebox.com' + element.get('href'), self.interval)
-            for element in etree.HTML(self.provider.get(
-                self.catalog, headers={'user-agent': self.user_agent}, proxy=True, mode=1
-            )).xpath('//a[@class="b-product-tile-image-link js-product-tile-link"]')
-            if 'nike' in element.get('href') or 'yeezy' in element.get('href') or 'jordan' in element.get('href')
-        ]
-
-    def execute(self, target: TargetType) -> StatusType:
-        try:
-            if isinstance(target, api.TInterval):
-                content: etree.Element = etree.HTML(
-                    self.provider.get(target.data, headers={'user-agent': self.user_agent},
-                                      proxy=True, mode=1))
-            else:
-                return api.SFail(self.name, 'Unknown target type')
-        except etree.XMLSyntaxError:
-            return api.SFail(self.name, 'Exception XMLDecodeError')
-        try:
-            if content.xpath('//button[@class="f-pdp-button f-pdp-button-coming-soon f-pdp-button--lowercase"]')[0].get(
-                    'aria-label') == 'Coming Soon':
-                return api.SWaiting(target)
-        except IndexError:
-            pass
-        available_sizes = tuple(str(size.get('data-value')) + ' EU'
-                                for size in content.xpath('//a[@class="js-pdp-attribute-btn b-pdp-swatch-link '
-                                                          'js-pdp-attribute-btn--size"]'))
-        name = target.data.split('/')[5].split('%')[0].replace('_', ' ').upper()
-        if len(available_sizes) > 0:
-            return api.SSuccess(
-                self.name,
-                api.Result(
-                    name,
-                    target.data.replace('de_DE', 'en_RU'),
-                    'solebox',
-                    content.xpath('//meta[@property="og:image"]')[0].get('content'),
-                    '',
-                    (api.currencies['EUR'], float(content.xpath('//span[@class="b-product-tile-price-item"]')[0].text
-                                                  .replace('€', '').replace('\n', '').replace(' ', '').split(',')[0])),
-                    {},
-                    available_sizes,
-                    (('StockX', 'https://stockx.com/search/sneakers?s=' + name.replace(' ', '%20')),
-                     ('Cart', 'https://www.solebox.com/cart'),
-                     ('Feedback', 'https://forms.gle/9ZWFdf1r1SGp9vDLA'))
-                )
+    def execute(self, mode: int, content: Union[CatalogType, TargetType]) -> List[
+        Union[CatalogType, TargetType, RestockTargetType, ItemType, TargetEndType]]:
+        result = [content]
+        if mode == 0:
+            links = []
+            c = self.provider.get(
+                self.link, headers=self.headers, proxy=True, mode=1
             )
-        else:  # TODO Fix when item is soldout
-            return api.SSuccess(
-                self.name,
-                api.Result(
-                    f'Sold Out {name}',
-                    target.data,
-                    'tech',
-                    '',
-                    '',
-                    (api.currencies['EUR'], 1.0),
-                    {},
-                    tuple(),
-                    (('StockX', 'https://stockx.com/search/sneakers?s=' + name.replace(' ', '%20')),
-                     ('Feedback', 'https://forms.gle/9ZWFdf1r1SGp9vDLA'))
-                )
-            )
+            print(c)
+            page_content = etree.HTML(c)
+            for element in page_content.xpath('//a[@class="b-product-tile-image-link js-product-tile-link"]'):
+                if 'yeezy' in element.get('href') or 'air' in element.get('href') or 'sacai' in element.get('href') \
+                        or 'dunk' in element.get('href') or 'retro' in element.get('href'):
+                    result.append(api.TInterval('https://www.solebox.com' + element.get('href'), self.name, 0, 3.))
+            return result
+        elif mode == 1:
+            page_content: etree.Element = etree.HTML(
+                self.provider.get(content.name, headers=self.headers,
+                                  proxy=True, mode=1))
+            name = page_content.xpath('//div[@class="js-target"]/a')[0].text.replace('\n', '').replace('\t', '') + \
+                   ' ' + page_content.xpath('//li[@class="b-breadcrumb-item"]/span')[0].text
+            try:
+                date = page_content.xpath('//button[@class="f-pdp-button f-pdp-button--release js-btn-release"]')[
+                    0].text
+            except IndexError:
+                date = 'Date not indicated'
+            try:
+                if page_content.xpath(
+                        '//button[@class="f-pdp-button f-pdp-button-coming-soon f-pdp-button--lowercase"]') \
+                        or page_content.xpath('//button[@class="f-pdp-button f-pdp-button--release js-btn-release"]'):
+                    result.append(
+                        api.IAnnounce(
+                            content.name,
+                            'solebox',
+                            name,
+                            page_content.xpath('//meta[@property="og:image"]')[0].get('content'),
+                            '',
+                            api.Price(api.CURRENCIES['EUR'], float(
+                                page_content.xpath('//span[@class="b-product-tile-price-item"]')[0].text.replace('€',
+                                                                                                                 '').replace(
+                                    '\n', '').replace(' ', '').split(',')[0])),
+                            api.Sizes(api.SIZE_TYPES[''], []),
+                            [
+                                FooterItem('StockX', 'https://stockx.com/search/sneakers?s=' +
+                                           name.replace(' ', '%20').replace('"', '').replace('\n', '')),
+                                FooterItem('Cart', 'https://www.solebox.com/cart'),
+                                FooterItem('Feedback', 'https://forms.gle/9ZWFdf1r1SGp9vDLA')
+                            ],
+                            {'Site': 'Solebox', 'Date': date}
+                        )
+                    )
+                else:
+                    sizes = [api.Size(size.get('data-value') + ' EU', size.get('href'))
+                             for size in
+                             page_content.xpath('//a[@class="js-pdp-attribute-btn b-pdp-swatch-link '
+                                                'js-pdp-attribute-btn--size"]')]
+                    name = content.name.split('/')[5].split('%')[0].replace('_', ' ').upper()
+                    if sizes:
+                        result.append(IRelease(
+                            content.name,
+                            'solebox',
+                            name,
+                            page_content.xpath('//meta[@property="og:image"]')[0].get('content'),
+                            '',
+                            api.Price(api.CURRENCIES['EUR'], float(
+                                page_content.xpath('//span[@class="b-product-tile-price-item"]')[0].text.replace('€',
+                                                                                                                 '').replace(
+                                    '\n', '').replace(' ', '').split(',')[0])),
+                            api.Sizes(api.SIZE_TYPES[''], sizes),
+                            [
+                                FooterItem('StockX', 'https://stockx.com/search/sneakers?s=' +
+                                           name.replace(' ', '%20').replace('"', '').replace('\n', '')),
+                                FooterItem('Cart', 'https://www.solebox.com/cart/cart'),
+                                FooterItem('Feedback', 'https://forms.gle/9ZWFdf1r1SGp9vDLA')
+                            ],
+                            {'Site': 'Solebox'}
+                        )
+                        )
+                        result.append(api.TESuccess(content, 'Item was Released'))
+
+            except etree.XMLSyntaxError:
+                raise etree.XMLSyntaxError('XMLDecodeError')
+        return result
