ec74c948026f961695a1bb5a5e55f6bbde868ff3
duckha <vaneegorovu21@gmail.com>
2020-07-15 19:31:41 +0300

    Optimized belief

--- belief/__init__.py
+++ belief/__init__.py
@@ -1,7 +1,6 @@
 from datetime import datetime, timedelta, timezone
 from typing import List, Union
 
-from lxml import etree
 from scripts.keywords_finding import check_name
 import yaml
 
@@ -16,7 +15,7 @@ from source.tools import LinearSmart
 class Parser(api.Parser):
     def __init__(self, name: str, log: logger.Logger, provider_: SubProvider):
         super().__init__(name, log, provider_)
-        self.link: str = 'https://beliefmoscow.com/collection/frontpage?order=&q='
+        self.link: str = 'https://beliefmoscow.com/collection/frontpage.json?order=&q=adidas,nike,jordan,yeezy'
         self.interval: int = 1
 
         raw = yaml.safe_load(open('./scripts/keywords.yaml'))
@@ -49,45 +48,48 @@ class Parser(api.Parser):
             content: Union[CatalogType, TargetType]
     ) -> List[Union[CatalogType, TargetType, RestockTargetType, ItemType, TargetEndType]]:
         result = []
-        if mode == 0:
-            links = []
-            for element in etree.HTML(self.provider.request(self.link, headers={'user-agent': self.user_agent}).text) \
-                    .xpath('//a[@class="product_preview-link"]'):
-                if check_name(element.get('href'), self.absolute_keywords,
-                              self.positive_keywords, self.negative_keywords):
-                    links.append(api.Target('https://beliefmoscow.com' + element.get('href'), self.name, 0))
 
-            for link in links:
-                try:
-                    if HashStorage.check_target(link.hash()):
-                        page_content: etree.Element = etree.HTML(
-                            self.provider.request(link.name, headers={'user-agent': self.user_agent}).text)
-                        try:
-                            sizes = [
-                                api.Size(str(size_data.text).split(' /')[0],
-                                         f"http://static.sellars.cf/links?site=belief&id={size_data.get('value')}")
-                                for size_data in
-                                page_content.xpath('//select[@id="variant-select"]')[0].xpath('option')
-                            ]
-                        except IndexError:
-                            HashStorage.add_target(link.hash())
-                            continue
-                        name = page_content.xpath('//meta[@property="og:title"]')[0].get('content')
-                        HashStorage.add_target(link.hash())
+        if mode == 0:
+
+            page_content = self.provider.request(self.link, headers={'user-agent': self.user_agent}).json()
+
+            for product in page_content['products']:
+
+                if check_name(product['permalink'], self.absolute_keywords, self.positive_keywords,
+                              self.negative_keywords) \
+                        or check_name(product['title'].lower(),
+                                      self.absolute_keywords, self.positive_keywords, self.negative_keywords):
+
+                    target = api.Target(f'https://beliefmoscow.com{product["url"]}', self.name, 0)
+
+                    if HashStorage.check_item(target.hash()):
+
+                        url = f'https://beliefmoscow.com{product["url"]}'
+                        name = product['title']
+                        price = api.Price(
+                            api.CURRENCIES['RUB'],
+                            float(product['variants'][0]['price'])
+
+                        )
+                        image = product['images'][0]['medium_url'] if len(product['images']) != 0 \
+                            else 'http://via.placeholder.com/300/2A2A2A/FFF?text=No+image'
+
+                        sizes = api.Sizes(api.SIZE_TYPES[''],
+                                          [api.Size(f"{size['title'].split(' /')[0]} [{size['quantity']}]",
+                                                    f"http://static.sellars.cf/links?site=belief&id={size['id']}")
+                                           for size in product['variants'] if size['quantity'] > 0])
+
+                        HashStorage.add_target(target.hash())
 
                         result.append(
                             IRelease(
-                                link.name,
+                                url,
                                 'belief',
                                 name,
-                                page_content.xpath('//meta[@property="og:image"]')[0].get('content'),
+                                image,
                                 '',
-                                api.Price(
-                                    api.CURRENCIES['RUB'],
-                                    float(page_content.xpath('//div[@class="product-page__price"]')[0].text.replace(
-                                        '₽', '').replace('\n', '').replace(' ', ''))
-                                ),
-                                api.Sizes(api.SIZE_TYPES[''], sizes),
+                                price,
+                                sizes,
                                 [
                                     FooterItem('StockX', 'https://stockx.com/search/sneakers?s=' +
                                                name.replace(' ', '%20')),
@@ -97,8 +99,6 @@ class Parser(api.Parser):
                                 {'Site': 'Belief Moscow'}
                             )
                         )
-                except etree.XMLSyntaxError:
-                    raise etree.XMLSyntaxError('Exception XMLDecodeError')
             if result or content.expired:
                 content.gen.time = self.time_gen()
                 content.expired = False
