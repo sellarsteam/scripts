142cb816c472f4a546536f6111dbae57de07027b
duckha <vaneegorovu21@gmail.com>
2020-10-08 22:10:12 +0300

    Removed encoding in raffle-monitor

--- raffle_monitor/__init__.py
+++ raffle_monitor/__init__.py
@@ -62,7 +62,6 @@ class Parser(api.Parser):
         self.headers = {
             'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:71.0) Gecko/20100101 Firefox/71.0',
             'Accept-Language': 'en-US,en;q=0.5',
-            'Accept-Encoding': 'gzip, deflate, br',
             'Connection': 'keep-alive',
             'Content-Type': 'application/json',
         }
