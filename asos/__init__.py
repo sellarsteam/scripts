52fca92e3b322d7b453717a9c17f774fc22a6556
duckha <vaneegorovu21@gmail.com>
2021-03-24 20:19:20 +0300

    Asos: Fix

--- asos/__init__.py
+++ asos/__init__.py
@@ -5,6 +5,7 @@ from typing import List, Union
 
 import pycurl
 import yaml
+from ujson import loads
 
 from source import api
 from source import logger
@@ -17,20 +18,54 @@ from source.tools import LinearSmart, ScriptStorage
 class Parser(api.Parser):
     def __init__(self, name: str, log: logger.Logger, provider_: SubProvider, storage: ScriptStorage, kw: Keywords):
         super().__init__(name, log, provider_, storage, kw)
-        self.men_link = "https://www.asos.com/api/product/search/v2/categories/4209?brand=14269,2986,13623,15177&channel=desktop-web&country=GB&currency=GBP&keyStoreDataversion=3pmn72e-27&lang=en-GB&limit=300&offset=0&rowlength=4&sort=freshness&store=COM"
-        self.women_link = "https://www.asos.com/api/product/search/v2/categories/4172?brand=14269,2986,15177&channel=desktop-web&country=GB&currency=GBP&keyStoreDataversion=3pmn72e-27&lang=en-GB&limit=300&offset=0&rowlength=4&store=COM"
+# https://www.asos.com/api/product/search/v2/categories/6455?attribute_1046=8620&channel=desktop-web&country=PL&currency=GBP&keyStoreDataversion=hnm9sjt-28&lang=pl-PL&limit=72&offset=0&rowlength=4&store=PL
+        # RU region
+        self.ru_men_link = "https://www.asos.com/api/product/search/v2/categories/4172?attribute_1047=8606" \
+                           "&brand=14269,2986,13623&country=RU&currency=RUB&lang=ru-RU&limit=100&store=RU&keyStoreDataversion=3pmn72e-27"
+        self.ru_women_link = "https://www.asos.com/api/product/search/v2/categories/4172?attribute_1047=8606" \
+                             "&brand=14269,2986&country=RU&currency=RUB&lang=ru-RU&limit=100&store=RU&keyStoreDataversion=3pmn72e-27"
+        self.ru_aj1_search_link = "https://www.asos.com/api/product/search/v2/?attribute_10992=61388&country" \
+                                  "=RU&currency=RUB&lang=ru-RU&limit=100&q=air%20jordan%201&store=RU&keyStoreDataversion=3pmn72e-27"
+        self.ru_dunk_search_link = "https://www.asos.com/api/product/search/v2/?attribute_1047=8606&attribut" \
+                                   "e_10992=61388&country=RU&currency=RUB&lang=ru-RU&limit=100&q=dunk&store=RU&keyStoreDataversion=3pmn72e-27"
+
+        # GB/COM region
+        self.en_women_link = "https://www.asos.com/api/product/search/v2/categories/4172?attribute_1047=8606&b" \
+                             "rand=14269,2986&country=GB&currency=GBP&lang=en-GB&limit=100&store=COM&keyStoreDataversion=3pmn72e-27"
+        self.en_men_link = "https://www.asos.com/api/product/search/v2/categories/4209?attribute_1047=8606&bra" \
+                           "nd=14269,2986,13623&country=GB&currency=GBP&lang=en-GB&limit=100&store=COM&keyStoreDataversion=3pmn72e-27"
+        self.en_aj1_search_link = "https://www.asos.com/api/product/search/v2/?attribute_10992=61388&country=GB" \
+                                  "&currency=GBP&lang=en-GB&limit=100&q=air%20jordan%201&store=COM&keyStoreDataversion=3pmn72e-27"
+        self.en_dunk_search_link = "https://www.asos.com/api/product/search/v2/?attribute_1047=8606&attribute_1" \
+                                   "0992=61388&country=GB&currency=GBP&lang=en-GB&limit=100&q=dunk&store=COM&keyStoreDataversion=3pmn72e-27"
+
+        # Poland
+        self.pl_men_link = "https://www.asos.com/api/product/search/v2/categories/4172?attribute_1047=8606" \
+                           "&brand=14269,2986,13623&country=PL&currency=GBP&lang=pl-PL&limit=100&store=PL&keyStoreDataversion=3pmn72e-27"
+        self.pl_women_link = "https://www.asos.com/api/product/search/v2/categories/4172?attribute_1047=8606" \
+                             "&brand=14269,2986&country=PL&currency=GBP&lang=pl-PL&limit=100&store=PL&keyStoreDataversion=3pmn72e-27"
+        self.pl_aj1_search_link = "https://www.asos.com/api/product/search/v2/?attribute_10992=61388&country" \
+                                  "=PL&currency=GBP&lang=pl-PL&limit=100&q=air%20jordan%201&store=PL&keyStoreDataversion=3pmn72e-27"
+        self.pl_dunk_search_link = "https://www.asos.com/api/product/search/v2/?attribute_1047=8606&attribut" \
+                                   "e_10992=61388&country=PL&currency=GBP&lang=pl-PL&limit=100&q=dunk&store=PL&keyStoreDataversion=3pmn72e-27"
+
+        # Products links
+        self.en_product_link = "https://www.asos.com/api/product/catalogue/v3/products/PRODUCT_ID" \
+                               "?store=COM&currency=GBP&keyStoreDataversion=3pmn72e-27"
+        self.ru_product_link = "https://www.asos.com/api/product/catalogue/v3/products/PRODUCT_ID" \
+                               "?store=RU&currency=RUB&keyStoreDataversion=3pmn72e-27"
+        self.pl_product_link = "https://www.asos.com/api/product/catalogue/v3/products/PRODUCT_ID" \
+                               "?store=PL&currency=RUB&keyStoreDataversion=3pmn72e-27"
+
         self.headers = {
-            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:71.0) Gecko/20100101 Firefox/71.0'
+            'user-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:71.0) Gecko/20100101 Firefox/71.0'
         }
 
-        self.product_link: str = "https://api.asos.com/product/catalogue/v3/products/PRODUCT_ID?sizeSchema=UK&store=COM&keyStoreDataversion=3pmn72e-27&currency=GBP&lang=en-GB"
-
-        self.interval: int = 1
         if self.storage.check('secret.yaml'):
             raw = yaml.safe_load(self.storage.file('secret.yaml'))
             if isinstance(raw, dict):
                 if 'pids' in raw and isinstance(raw['pids'], list):
-                        self.pids = [k for k in raw['pids']]
+                    self.pids = [k for k in raw['pids']]
                 else:
                     raise IndexError('secret.yaml must contain pids (as object)')
             else:
@@ -58,83 +93,119 @@ class Parser(api.Parser):
 
         if mode == 0:
             result.append(content)
-            ok_0, resp_0 = self.provider.request(self.men_link, headers=self.headers)
-            ok_1, resp_1 = self.provider.request(self.women_link, headers=self.headers)
+            result.append(api.TScheduled('ru_men_catalog', self.name,
+                                         ['catalog', self.ru_men_link, 'RUB', 'Russia :flag_ru:'], time()))
+            result.append(api.TScheduled('ru_women_catalog', self.name,
+                                         ['catalog', self.ru_women_link, 'RUB', 'Russia :flag_ru:'], time()))
+            result.append(api.TScheduled('ru_search_aj1_catalog', self.name,
+                                         ['catalog', self.ru_aj1_search_link, 'RUB', 'Russia :flag_ru:'], time()))
+            result.append(api.TScheduled('ru_search_dunk_catalog', self.name,
+                                         ['catalog', self.ru_dunk_search_link, 'RUB', 'Russia :flag_ru:'], time()))
 
-            if not ok_0:
-                if resp_0.args[0] == pycurl.E_OPERATION_TIMEOUTED:
-                    return result
-                else:
-                    raise resp_0
+            result.append(api.TScheduled('en_men_catalog', self.name,
+                                         ['catalog', self.en_men_link, 'GBP', 'UK :flag_gb:'], time()))
+            result.append(api.TScheduled('en_women_catalog', self.name,
+                                         ['catalog', self.en_women_link, 'GBP', 'UK :flag_gb:'], time()))
+            result.append(api.TScheduled('en_search_aj1_catalog', self.name,
+                                         ['catalog', self.en_aj1_search_link, 'GBP', 'UK :flag_gb:'], time()))
+            result.append(api.TScheduled('en_search_dunk_catalog', self.name,
+                                         ['catalog', self.en_dunk_search_link, 'GBP', 'UK :flag_gb:'], time()))
 
-            if not ok_1:
-                if resp_1.args[0] == pycurl.E_OPERATION_TIMEOUTED:
-                    return result
-                else:
-                    raise resp_1
-
-            catalog_0 = resp_0.json()
-            catalog_1 = resp_1.json()
+            result.append(api.TScheduled('pl_men_catalog', self.name,
+                                         ['catalog', self.pl_men_link, 'POL', 'PL :flag_pl:'], time()))
+            result.append(api.TScheduled('pl_women_catalog', self.name,
+                                         ['catalog', self.pl_women_link, 'POL', 'PL :flag_pl:'], time()))
+            result.append(api.TScheduled('pl_search_aj1_catalog', self.name,
+                                         ['catalog', self.pl_aj1_search_link, 'POL', 'PL :flag_pl:'], time()))
+            result.append(api.TScheduled('pl_search_dunk_catalog', self.name,
+                                         ['catalog', self.pl_dunk_search_link, 'POL', 'PL :flag_pl:'], time()))
 
             for pid in self.pids:
-                result.append(api.TScheduled(str(pid), self.name, [str(pid)], time()))
-
-            for c in catalog_0['products']:
-                if self.kw.check(c['name'].lower() + ' ' + str(c['id'])):
-                    result.append(
-                        api.TScheduled(
-                            str(c['id']),
-                            self.name,
-                            [c['url']],
-                            time()
-                        )
-                    )
-
-            for c in catalog_1['products']:
-                if self.kw.check(c['name'].lower() + ' ' + str(c['id'])):
-                    result.append(
-                        api.TScheduled(
-                            str(c['id']),
-                            self.name,
-                            [c['url']],
-                            time()
-                        )
-                    )
+                result.append(api.TScheduled(str(pid), self.name, ['item', 'GBP', 'UK :flag_gb:'], time()))
+                result.append(api.TScheduled(str(pid), self.name, ['item', 'RUB', 'RU :flag_ru:'], time()))
+                result.append(api.TScheduled(str(pid), self.name, ['item', 'POL', 'PL :flag_pl:'], time()))
 
             if result or content.expired:
                 content.gen.time = self.time_gen()
                 content.expired = False
 
         elif mode == 1:
-
-            ok, response = self.provider.request(self.product_link.replace('PRODUCT_ID', content.name),
-                                                 headers=self.headers)
-
-            if not ok:
-                if response.args[0] == pycurl.E_OPERATION_TIMEOUTED:
+            if content.data[0] == 'catalog':
+                ok, response = self.provider.request(content.data[1], headers=self.headers, proxy=True)
+                try:
+                    catalog = loads(response.text)
+                except (AttributeError, ValueError):
+                    result.append(result.append(api.MAlert(f'Script is down {content.name}', self.name)))
                     return result
+
+                if not ok:
+                    return result
+
+                for c in catalog['products']:
+
+                    if self.kw.check(c['name'].lower() + ' ' + str(c['id'])):
+                        result.append(
+                            api.TScheduled(
+                                str(c['id']),
+                                self.name,
+                                ['item', content.data[2], content.data[3]],
+                                time()
+                            )
+                        )
+
+            elif content.data[0] == 'item':
+                if content.data[1] == 'RUB':
+                    product_url = self.ru_product_link
+                    currency = 'RUB'
+                    region = 'ru'
+                elif content.data[1] == 'GBP':
+                    product_url = self.en_product_link
+                    currency = 'GBP'
+                    region = 'gb'
                 else:
-                    raise result
+                    product_url = self.pl_product_link
+                    currency = 'RUB'
+                    region = 'pl'
 
-            json_data = response.json()
+                ok, response = self.provider.request(product_url.replace('PRODUCT_ID', content.name),
+                                                     headers=self.headers, proxy=True)
 
-            try:
+                if not ok:
+                    if response.args[0] == pycurl.E_OPERATION_TIMEOUTED:
+                        return result
+                    else:
+                        raise result
+                try:
+                    json_data = response.json()
+                except ValueError:
+                    result.append(result.append(api.MAlert(f'Script is down {content.name}', self.name)))
+                    return result
 
-                name = json_data["name"]
-                link = f'https://asos.com/{content.data[0]}'
+                try:
+                    name = json_data["name"]
+                except KeyError:
+                    if json_data['errorCode'] == 'pdt_011':
+                        return result
+
+                link = f'https://asos.com/{region}/prd/{content.name}'
                 image = f"https://images.weserv.nl/?url={json_data['media']['images'][0]['url']}"
 
-                row_sizes = [
-                    api.Size(sku['brandSize'])
-                    for sku in json_data['variants']
-                    if sku['isInStock'] is True
-                ]
+                raw_sizes = []
 
-                sizes = api.Sizes(api.SIZE_TYPES[''], row_sizes)
-                price = api.Price(api.CURRENCIES['GBP'],
+                for sku in json_data['variants']:
+                    if sku['isInStock']:
+                        if sku['isLowInStock']:
+                            raw_sizes.append(
+                                api.Size(f"{sku['brandSize']} [LOW]"))
+                        else:
+                            raw_sizes.append(
+                                api.Size(f"{sku['brandSize']} [HIGH]"))
+
+                sizes = api.Sizes(api.SIZE_TYPES[''], raw_sizes)
+                price = api.Price(api.CURRENCIES[currency],
                                   float(json_data['price']['current']['value']))
 
-                if row_sizes:
+                if raw_sizes:
                     result.append(
                         IRelease(
                             f'{link}?shash={sizes.hash().hex()}',
@@ -146,15 +217,13 @@ class Parser(api.Parser):
                             sizes,
                             [
                                 FooterItem('Login', 'https://my.asos.com/'),
-
                                 FooterItem('Cart', 'https://www.asos.com/bag')
                             ],
                             {
-                                'Site': '[ASOS](https://asos.com)'
+                                'Site': '[ASOS](https://asos.com)',
+                                'Region': content.data[2]
                             }
                         )
                     )
-            except KeyError:
-                pass
 
         return result
